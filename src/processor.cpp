9e7ba7fcfd6b6cb16851e24ffb1d8eb254b86767
Joao <silva.joaomatos@gmail.com>
2021-01-07 00:03:21 +0100

    cpu utilization ongoing

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,13 @@
 #include "processor.h"
-
+#include "linux_parser.h"
+#include <dirent.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+    std::vector<std::string> cpu= LinuxParser::CpuUtilization();
+    float cpuLoad{10.0};
+    cpuLoad = 100.0-std::stof(cpu[3]);   
+    return cpuLoad;
+}
